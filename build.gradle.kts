a45c02d50cd0efbfcc762e3359ed85b4111cffaa
alpermelkeli <alpermelkeli@gmail.com>
2024-08-07 13:09:30 +0300

    Finished.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,13 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "org.example.VoteApp"
+        )
+    }
+}
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
